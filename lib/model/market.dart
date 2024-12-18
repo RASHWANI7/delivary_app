20f61be30c78a972b9350a12325ed83b98dfdf50
abode <aboderashwani7@gmail.com>
2024-12-18 10:17:21 +0000

    first commit

---
+++ lib/model/market.dart
@@ -0,0 +1,11 @@
+class Markets{
+  final int id;
+  final String name;
+  final String photo_url;
+
+  Markets({
+    required this.id,
+    required this.name,
+    required this.photo_url
+  });
+}
